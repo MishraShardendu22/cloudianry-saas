45d0b5390c86d41de7994ddf8aec9cf29d837d67
Shardendu Mishra <152168607+ShardenduMishra22@users.noreply.github.com>
2024-09-29 15:49:46 +0530

    Worrking on Front

--- src/app/(app)/home/page.tsx
+++ src/app/(app)/home/page.tsx
@@ -1,3 +1,10 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+"use client"
+import { useState,useEffect,useCallback } from 'react'
+import axios from "axios";
+import VideoCard from '../../components/VideoCard'
+import { Video } from '../../types/index'
+
 function Home() {
     return (
         <div>
